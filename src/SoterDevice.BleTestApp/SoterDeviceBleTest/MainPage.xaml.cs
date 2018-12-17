e5ca070549a09bdc284c59d2c51c7c60d926f282
Zhen Liu <liuzhen@touchjet.com>
2018-12-17 18:01:22 +0800

    Initialize device on item click.

--- src/SoterDevice.BleTestApp/SoterDeviceBleTest/MainPage.xaml.cs
+++ src/SoterDevice.BleTestApp/SoterDeviceBleTest/MainPage.xaml.cs
@@ -17,14 +17,21 @@ namespace SoterDeviceBleTest
             base.OnAppearing();
         }
 
-        async void ButtonScan_ClickedAsync(object sender, System.EventArgs e)
+        async void ButtonScan_ClickedAsync(object sender, EventArgs e)
         {
             await SoterDeviceFactoryBle.Instance.StartDeviceSearchAsync();
             foreach(var device in SoterDeviceFactoryBle.Instance.Devices)
             {
                 Console.WriteLine($"Found device: {device.Name}");
-                Device.BeginInvokeOnMainThread(async () => await device.InitializeAsync());
             }
+        }
+
+        async void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var device = (SoterDeviceBle)e.SelectedItem;
+            await SoterDeviceFactoryBle.Instance.StopDeviceSearchAsync();
+            await device.InitializeAsync();
+            var coinTable = await device.GetCoinTable();
         }
     }
 }
