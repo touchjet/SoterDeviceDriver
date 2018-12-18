feb2a24094944f78e367bbd81e28cd5d93e2f629
Zhen Liu <liuzhen@touchjet.com>
2018-12-18 15:46:58 +0800

    Add console program to test HID interface.

---
+++ src/SoterDevice.Hid.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace SoterDevice.Hid.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .WriteTo.Console(Serilog.Events.LogEventLevel.Debug)
+                        .CreateLogger();
+            ResetDevice().Wait();
+        }
+
+        static async Task ResetDevice()
+        {
+            await SoterDeviceFactoryHid.Instance.StartDeviceSearchAsync();
+            await Task.Delay(3000);
+            await SoterDeviceFactoryHid.Instance.StopDeviceSearchAsync();
+
+            var _soterDevice = (SoterDeviceHid)SoterDeviceFactoryHid.Instance.Devices.FirstOrDefault();
+            _soterDevice.EnterPinCallback = _soterDevice_EnterPinCallback;;
+            await _soterDevice.InitializeAsync();
+            var coinTable = await _soterDevice.GetCoinTable();
+        }
+
+        static Task<string> _soterDevice_EnterPinCallback()
+        {
+            var passStr = System.Console.ReadLine();
+            return Task.FromResult(passStr);
+        }
+
+    }
+}
