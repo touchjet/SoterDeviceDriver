baeb771b17a8c49aa53159771658a2f8deb63d60
Zhen Liu <liuzhen@touchjet.com>
2019-01-29 22:04:38 +0800

    Update Biginteger with ulong.

--- src/SoterDevice/Models/IEthereumTransaction.cs
+++ src/SoterDevice/Models/IEthereumTransaction.cs
@@ -23,8 +23,8 @@ namespace SoterDevice.Models
     public interface IEthereumTransaction : ITransaction
     {
         uint ChainId { get; }
-        BigInteger GasPrice { get; }
-        BigInteger GasLimit { get; }
+        ulong GasPrice { get; }
+        ulong GasLimit { get; }
         BigInteger Nonce { get; }
         byte[] Data { get; }
     }
